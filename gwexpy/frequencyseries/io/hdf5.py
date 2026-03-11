6c248548b87b6be104770b5dedd85e47935afd15
Tatsuki Washimi <tatsuki.washimi@gmail.com>
2026-03-11 22:46:43 +0900

    Restore remaining gwpy shim exports

--- gwexpy/frequencyseries/io/hdf5.py
+++ gwexpy/frequencyseries/io/hdf5.py
@@ -1,1 +1,6 @@
 from __future__ import annotations
+
+from gwpy.frequencyseries.io import hdf5 as _gwpy_hdf5
+from gwpy.frequencyseries.io.hdf5 import *  # noqa: F403
+
+__all__ = getattr(_gwpy_hdf5, "__all__", [name for name in dir(_gwpy_hdf5) if not name.startswith("_")])
--- gwexpy/io/gwf.py
+++ gwexpy/io/gwf.py
@@ -1,1 +1,6 @@
 from __future__ import annotations
+
+from gwpy.io import gwf as _gwpy_gwf
+from gwpy.io.gwf import *  # noqa: F403
+
+__all__ = getattr(_gwpy_gwf, "__all__", [name for name in dir(_gwpy_gwf) if not name.startswith("_")])
