978676d66fdd2d89ff7a96da28adedd21d8ff030
Tatsuki Washimi <tatsuki.washimi@gmail.com>
2026-03-11 21:49:09 +0900

    Restore shim exports for IO and segment helpers

--- gwexpy/io/ligolw.py
+++ gwexpy/io/ligolw.py
@@ -1,1 +1,4 @@
 from __future__ import annotations
+
+from gwpy.io.ligolw import *  # noqa: F403
+from gwpy.io.ligolw import __all__
--- gwexpy/signal/window.py
+++ gwexpy/signal/window.py
@@ -1,1 +1,4 @@
 from __future__ import annotations
+
+from gwpy.signal.window import *  # noqa: F403
+from gwpy.signal.window import __all__
