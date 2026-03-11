5baed89da34e631c7588dc33ad9e608474305584
Tatsuki Washimi <tatsuki.washimi@gmail.com>
2026-03-11 21:49:09 +0900

    Restore shim exports for IO and segment helpers

--- gwexpy/io/hdf5.py
+++ gwexpy/io/hdf5.py
@@ -1,1 +1,4 @@
 from __future__ import annotations
+
+from gwpy.io.hdf5 import *  # noqa: F403
+from gwpy.io.hdf5 import __all__
--- gwexpy/segments/segments.py
+++ gwexpy/segments/segments.py
@@ -1,1 +1,4 @@
 from __future__ import annotations
+
+from gwpy.segments.segments import *  # noqa: F403
+from gwpy.segments.segments import __all__
