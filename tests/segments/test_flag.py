0ce711faf004d116b39367f92ca8677628e8cbbd
tatsuki-washimi <tatsuki.washimi@gmail.com>
2026-04-27 09:19:23 +0900

    ci: mark GWOSC segment fetch test as network

--- tests/segments/test_flag.py
+++ tests/segments/test_flag.py
@@ -1,1 +1,6 @@
+import pytest
 from gwpy.segments.tests.test_flag import *  # noqa: F403
+
+TestDataQualityFlag.test_fetch_open_data = pytest.mark.network(  # noqa: F405
+    TestDataQualityFlag.test_fetch_open_data  # noqa: F405
+)
